43d8ec31c134fa52d9cb87f927c0fdb7d1cedb09
santimassepal <santi.massepal@gmail.com>
2022-08-13 19:55:39 +0200

    Create moduloAg.hs

---
+++ moduloAg.hs
@@ -0,0 +1,37 @@
+
+
+
+--COMBINACIONES
+--------------------------------------------------------------------------------------------------------------------------
+combinacion1 :: [a] -> [a] -> Int -> [a] --Recibe dos cromosomas y la posicion a partir de la cual termina el primero y comienza el segundo
+combinacion1 xs ys i = combinacionAux xs ys i []
+
+combinacion1Aux :: [a] -> [a] -> Int -> [a] -> [a]
+combinacion1Aux [x] [y] i zs
+    | i==0 = zs ++ [y]
+    | otherwise = zs ++ [x]
+-- cuando las listas solo tienen un elemento se devuelve la última concatenación
+combinacionAux (x:xs) (y:ys) i zs 
+    |i==0 = combinacion1Aux xs ys 0 (zs++[y])
+    |otherwise = combinacion1Aux xs ys (i-1) (zs++[x])
+--------------------------------------------------------------------------------------------------------------------------
+     
+
+--MUTACIONES
+--------------------------------------------------------------------------------------------------------------------------
+mutacion1 :: [a] -> Int -> a -> [a]
+mutacion1 xs pos num = (take pos xs) ++ [num] ++ (reverse (take ((length xs)-(pos+1)) (reverse xs))) 
+--num es un numero aleatorio introducido como parametro de la función 
+--------------------------------------------------------------------------------------------------------------------------
+permutacioninter :: [a] -> Int -> Int -> [a]   --Recibe el cromosoma y las dos posiciones a intercambiar (importante introducir los numeros en orden)
+permutacioninter xs pos1 pos2 = (take pos1 xs) ++ [xs !! pos2] ++ (interludio xs pos1 pos2) ++ [xs !! pos1] ++ (drop (pos2+1) xs)
+
+interludio :: [a] -> Int -> Int -> [a]
+interludio xs pos1 pos2 = take (pos2-(pos1+1)) (drop (pos1+1) xs)
+--------------------------------------------------------------------------------------------------------------------------
+--permutacioninser :: [a] -> Int -> Int -> [a]   --Recibe el cromosoma la posición donde insertar el gen y la posicion del gen a insertar
+--permutacioninser = 
+--------------------------------------------------------------------------------------------------------------------------
+--permutacionmezcla :: [a] -> Int -> 
+--permutacionmezcla = 
+--------------------------------------------------------------------------------------------------------------------------
