13f25ac91e33bfea2efa664cda976983f89ce534
santimassepal <santi.massepal@gmail.com>
2022-08-25 18:04:49 +0200

    al fin funciona jeje

--- fitness.hs
+++ fitness.hs
@@ -14,7 +14,10 @@ valoresViajante :: Int -> [(Int,Int)] --Coordenadas que utilizaremos para el pro
 valoresViajante n = [(x,y) | x <- [0..n] , y <- [0..n] , (x==0 || y==0 || x==n || y==n)]
 
 fitnessProblemaViajante :: [Int] -> Double
-fitnessProblemaViajante xs = sum [distanciaManhattan ((valoresViajante 10)!!(xs!!x)) ((valoresViajante 10)!!(xs!!(x+1))) | x <- [0..((length xs)-1)]]
+fitnessProblemaViajante xs =  sum ([distanciaManhattan ((valoresViajante 10)!!(xs!!x)) ((valoresViajante 10)!!(xs!!(x+1))) | x <- [0..((length xs)-2)]]  
+                                ++  [distanciaManhattan ((valoresViajante 10) !! 0) ((valoresViajante 10) !! 39)])
 
 distanciaManhattan :: (Int,Int) -> (Int,Int) -> Double
 distanciaManhattan (a1,a2) (b1,b2) = sqrt ( fromIntegral(((a1-b1)^2)) + fromIntegral(((a2-b2)^2)) )
+
+
